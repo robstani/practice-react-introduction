e80a76804134e554f46a77ea7ec61568462c2a7d
Robert Staniszewski <robstani.tech@gmail.com>
2024-04-22 11:05:25 +0200

    Update react to version 18

--- 01/app.js
+++ 01/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'));
 
 const App = () => {
     return (
@@ -7,4 +9,4 @@ const App = () => {
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
---
+++ 02/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+            <header>React działa</header>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
