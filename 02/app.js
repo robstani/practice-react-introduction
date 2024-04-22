606e4a6d9548705586ebdf450306b5ed724e073f
Robert Staniszewski <robstani.tech@gmail.com>
2024-04-22 11:20:07 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'));
+
+/* const App = () => {
+    const headerStyle = {
+        color: '#333', 
+        backgroundColor: '#f3f3f3', 
+        padding: '10px', 
+        borderRadius: '5px', 
+        textAlign: 'center',
+        fontSize: '1.5em',
+    };
+    
+    return (
+            <header style={headerStyle}>Moja pierwsza strona w React.</header>
+    );
+}; */
+
+class Header extends React.Component {
+    render() {
+        const headerStyle = {
+            color: '#555', 
+            backgroundColor: '#f3f3f3', 
+            padding: '10px', 
+            borderRadius: '5px', 
+            textAlign: 'center',
+            fontSize: '1.5em',
+        };
+        
+        return (
+                <header style={headerStyle}>Moja pierwsza strona w React.</header>
+        );
+    }
+}
 
 const App = () => {
     return (
-            <header>React działa</header>
+        <div>
+            <Header />
+        </div>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
